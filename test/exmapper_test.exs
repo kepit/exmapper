4947001cb651fa49efcc33e2916fca318a43abec
Sampo Vuori <sampo@kepit.fi>
2016-10-25 13:55:26 +0000

    needs some tests

--- test/exmapper_test.exs
+++ test/exmapper_test.exs
@@ -5,3 +5,14 @@ defmodule ExmapperTest do
     assert 1 + 1 == 2
   end
 end
+
+defmodule TestData do
+  use Exmapper.Model, repo: :exmapper_test
+  schema "test_data" do
+    field :strindata, :string
+    field :jsondata, :json
+    field :integerdata, :integer
+    field :booleandata, :boolean
+    field :datatimedata, :datetime
+  end
+end
